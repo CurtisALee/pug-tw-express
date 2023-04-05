60899b62a953d5964e80b4b7dc3cdf27fe1329b0
Loic at Nuom <71375865+LoicNuom@users.noreply.github.com>
2023-04-05 17:45:24 +0000

    Alternative using the render engine

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ const path = require('path');
 const glob = require('glob');
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'pages'));
 
 app.use(express.static('public'));
 
 glob.sync('./pages/**/*.pug').forEach((file) => {
   const name = path.basename(file, '.pug');
   app.get('/' + name, (req, res) => {
-    res.send(pug.renderFile('pages/' + name+'.pug'));
+    res.render(name);
   });
 });
 
-app.get('/', (req, res) => {
-  res.redirect('/index');
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
 });
+
 
 
 const PORT = process.env.PORT || 3000;
